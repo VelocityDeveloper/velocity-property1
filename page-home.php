3c3d98d33578457cc97df599a63684b97cb74dc1
irawanyulianto <71108779+irawanyulianto@users.noreply.github.com>
2025-09-23 15:23:52 +0700

    perapian thumbnail di page-home.php

--- page-home.php
+++ page-home.php
@@ -52,13 +52,13 @@ get_header();
     $wp_query = new WP_Query($args); 
     global $product_metabox;
     if($wp_query->have_posts ()): ?>
-    <div class="text-center mb-3 mt-5">
+    <div class="text-center mb-3 mt-5 ">
     <h3 class="w-100 text-center mt-5 mb-1 d-inline-block"><?php echo velocitytheme_option('judul_properti', ''); ?></h3>
     <p class="w-100 text-center mb-3 d-inline-block text-secondary"><?php echo velocitytheme_option('subjudul_properti', ''); ?></p>
     </div>
-    <div class="text-center row">
+    <div class="text-center row property">
     <?php while($wp_query->have_posts()): $wp_query->the_post(); ?>
-    <div class="col-sm-4 mb-5">
+    <div class="col-sm-4 mb-5 ">
         <?php get_template_part('property', 'loop');?>
     </div>
     <?php endwhile; ?>
@@ -86,17 +86,25 @@ get_header();
     $content = get_the_content();
     $trimmed_content = wp_trim_words($content,20); ?>
     <div class="col-sm-4">
-    <div class="card w-100 text-start rounded-0 border-0 bg-transparent">
-    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-    <img class="home-bottom-post-thumb rounded-0" src="<?php the_post_thumbnail_url('large'); ?>" >
-    </a>
-    <div class="card-body px-0">
-        <div class="text-dark"><?php echo get_the_date(); ?></div>
-        <h5 class="card-title mb-2" style="line-height: 1.4;"><a class="text-dark" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h5>
-        <p class="card-text text-secondary"><?php echo $trimmed_content; ?></p>
-        <div class="mt-3">By <?php echo get_the_author_meta('display_name'); ?></div>
-    </div>
-    </div>
+        <div class="card w-100 text-start rounded-0 border-0 bg-transparent">
+        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+            <div class="ratio ratio-4x3">
+            <img class="home-bottom-post-thumb rounded-0 img-fluid" 
+                src="<?php the_post_thumbnail_url('large'); ?>" 
+                alt="<?php the_title_attribute(); ?>">
+            </div>
+        </a>
+        <div class="card-body px-0">
+            <div class="text-dark"><?php echo get_the_date(); ?></div>
+            <h5 class="card-title mb-2" style="line-height: 1.4;">
+                <a class="text-dark" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                <?php the_title(); ?>
+                </a>
+            </h5>
+            <p class="card-text text-secondary"><?php echo $trimmed_content; ?></p>
+            <div class="mt-3">By <?php echo get_the_author_meta('display_name'); ?></div>
+        </div>
+        </div>
     </div>
     <?php endwhile; ?>
     </div>
