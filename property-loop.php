cbd48ff5a0fc1924725851475ee1ca22dcd858d7
irawanyulianto <71108779+irawanyulianto@users.noreply.github.com>
2025-09-23 15:25:10 +0700

    perapian thumbnail dg aspect ratio

--- property-loop.php
+++ property-loop.php
@@ -4,16 +4,25 @@ $trimmed_content = wp_trim_words($content,10);
 ?>
 
 <div class="card h-100 w-100 text-start rounded-0">
-    <?php $kondisi_property = get_post_meta(get_the_ID(), 'kondisi_property', true );
+    <?php 
+    $kondisi_property = get_post_meta(get_the_ID(), 'kondisi_property', true );
     if($kondisi_property){ ?>
         <div class="label-property"><?php echo $kondisi_property; ?></div>
     <?php } ?>
+    
     <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-        <img class="card-img-top m-0 rounded-0" src="<?php the_post_thumbnail_url('large'); ?>" >
+        <div class="ratio ratio-4x3">
+            <img class="card-img-top m-0 rounded-0 img-fluid" 
+                 src="<?php the_post_thumbnail_url('large'); ?>" 
+                 alt="<?php the_title_attribute(); ?>">
+        </div>
     </a>
+    
     <div class="card-body">
         <h5 class="card-title fs-5">
-            <a class="text-dark" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+            <a class="text-dark" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                <?php the_title(); ?>
+            </a>
         </h5>
         <p class="card-text text-secondary"><?php echo $trimmed_content; ?></p>
         <div class="text-dark">Harga:</div>
